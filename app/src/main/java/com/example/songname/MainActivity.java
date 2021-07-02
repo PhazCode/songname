e395391a0878dab5ac67a0835c93f9552609a8b4
Guillaume Couairon <gcouairon@fb.com>
2021-07-02 11:19:39 +0200

    hello

---
+++ app/src/main/java/com/example/songname/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.songname;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
